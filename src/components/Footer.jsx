068e0f70cc4ad1126a3c913a56f12975e10879e1
Lavan0927 <86250702+Lavan0927@users.noreply.github.com>
2021-06-25 00:56:00 +0530

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <footer>
+        <p>Copyright to Shapeai @{new Date().getFullYear()}</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <header>
+        <h1>Shape AI Bootcamp</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
